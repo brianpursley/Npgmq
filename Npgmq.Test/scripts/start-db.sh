fa8f8eb1c7635aaa3db79f4091a7686b365c405a
Brian Pursley <bpursley@cinlogic.com>
2024-04-16 12:39:03 -0400

    Add build workflow

--- Npgmq.Test/scripts/start-db.sh
+++ Npgmq.Test/scripts/start-db.sh
@@ -7,7 +7,7 @@ docker run -d -it --name npgmq_test_db -p 5432:5432 --rm quay.io/tembo/tembo-loc
 
 docker exec npgmq_test_db /bin/sh -c "psql -c \"CREATE DATABASE npgmq_test;\""
 
-if [ -z "$PGMQ_VERSION" ]; then
+if [ -z "$PGMQ_VERSION" ] || [ "$PGMQ_VERSION" = "latest" ]; then
   docker exec npgmq_test_db /bin/sh -c "trunk install pgmq"
 else
   docker exec npgmq_test_db /bin/sh -c "trunk install pgmq --version=${PGMQ_VERSION}"
