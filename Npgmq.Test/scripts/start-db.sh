38153640f0257548d2111a619765c4dd0c6d9d02
Brian Pursley <bpursley@cinlogic.com>
2024-05-22 09:41:05 -0400

    Add sleep to start-db.sh, to avoid occasional flake due to docker startup time.

--- Npgmq.Test/scripts/start-db.sh
+++ Npgmq.Test/scripts/start-db.sh
@@ -4,6 +4,7 @@ set -e
 PGMQ_VERSION=$1 
 
 docker run -d --name npgmq_test_db -p 5432:5432 --rm quay.io/tembo/tembo-local
+sleep 4
 
 docker exec npgmq_test_db /bin/sh -c "psql -c \"CREATE DATABASE npgmq_test;\""
 
