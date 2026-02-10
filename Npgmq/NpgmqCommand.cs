559dffc0c571a73eef298e1bb5190abe9528ad4e
Brian Pursley <bpursley@cinlogic.com>
2026-02-10 10:56:02 -0500

    Remove unnecessary blank line in DisposeAsync method

--- Npgmq/NpgmqCommand.cs
+++ Npgmq/NpgmqCommand.cs
@@ -11,7 +11,7 @@ internal class NpgmqCommand(string commandText, NpgsqlConnection connection, boo
     public override async ValueTask DisposeAsync()
     {
         await base.DisposeAsync().ConfigureAwait(false);
-        
+
         if (disposeConnection && Connection != null && Interlocked.CompareExchange(ref _connectionHasBeenDisposed, 1, 0) == 0)
         {
             await Connection.DisposeAsync().ConfigureAwait(false);
